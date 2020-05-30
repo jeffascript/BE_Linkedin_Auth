789137c82c2374850271c1862abba6801aaed00a
jeffascript <ofoaro.jeff@gmail.com>
2020-05-30 23:09:28 +0200

    ++ post schema prettier

--- src/models/postSchema.js
+++ src/models/postSchema.js
@@ -1,70 +1,62 @@
 const mongoose = require("mongoose");
 
-
-
 const reactionSchema = {
-    likedBy:  {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "profiles"
-        //   type: String,
-        //   index:true, 
-        //   unique:true,
-        //   sparse:true
-      },
-      userID:{
-          type: mongoose.Schema.Types.ObjectId
-      }
+  likedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "profiles",
+    //   type: String,
+    //   index:true,
+    //   unique:true,
+    //   sparse:true
+  },
+  userID: {
+    type: mongoose.Schema.Types.ObjectId,
+  },
+};
 
-  };
+const schema = {
+  text: {
+    type: String,
+    required: true,
+  },
 
-  
-const schema = { 
+  username: {
+    type: mongoose.Schema.Types.String,
+    ref: "profiles",
+  },
+  userInfo: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "profiles",
+  },
 
-    text: {
-        type: String,
-        required: true
-        
-    },
+  image: {
+    type: String,
+    // default: "https://via.placeholder.com/150",
+    required: false,
+  },
+  // createdAt: {
+  //     type: Date,
+  //     default: Date.now,
+  //     required: false
+  // },
 
-    username: {
-        type: mongoose.Schema.Types.String,
-        ref: "profiles"
-        
-    },
-    userInfo: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "profiles"
-        
-    },
+  // updatedAt: {
+  //     type: Date,
+  //     default: Date.now,
+  //     required: false
+  // },
 
-    image: {
-        type: String,
-        // default: "https://via.placeholder.com/150",
-        required: false
-    },
-    // createdAt: {
-    //     type: Date,
-    //     default: Date.now,
-    //     required: false
-    // },
- 
-    // updatedAt: {
-    //     type: Date,
-    //     default: Date.now,
-    //     required: false
-    // },
+  reactions: [reactionSchema],
 
-    reactions:[reactionSchema],
-
-    comment: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Comment",
-        required: false
-    }
+  comment: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Comment",
+    required: false,
+  },
 };
 
 const collectionName = "posts";
-const postSchema = mongoose.Schema(schema,{timestamps:true});
+const postSchema = mongoose.Schema(schema, { timestamps: true });
 const Post = mongoose.model(collectionName, postSchema);
 
 module.exports = Post;
